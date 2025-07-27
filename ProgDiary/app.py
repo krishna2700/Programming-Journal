7cb1acbcc99bb986b6c5d9452f7514d9d22d3bff
krishna2700 <krishnaruparelia0207@gmail.com>
2025-07-27 13:16:24 +0530

    using python lists as an in-memory database

--- ProgDiary/app.py
+++ ProgDiary/app.py
@@ -1,3 +1,5 @@
+from database import add_entry , get_entries
+
 menu = """ Please select the following options:
 1. Add a new entry for today
 2. View entries
@@ -6,11 +8,22 @@ menu = """ Please select the following options:
 Your Selection: """
 welcome = "Welcome to the Programming Diary!"
 
+def prompt_new_entry(): 
+     entry_content = input("What have you learned today? ") # Prompt user for entry content
+     entry_date = input("Enter the date (YYYY-MM-DD): ") # Prompt user for entry date 
+     add_entry(entry_content, entry_date) # Add a new entry to the diary database
+
+def view_entries(entries):
+    for entry in entries: # Loop through each entry in the list
+        print(f"{entry['date']}\n{entry['content']}\n\n") # Display each entry with a newline for readability
+     
+
 while (user_input := input(menu)) !="3": # Loop until user selects '3' to exit
     if user_input == "1":
-        print("Adding...")
+        prompt_new_entry() # Prompt user to add a new entry
     elif user_input == "2":
-        print("Viewing...")
+        entries = get_entries() # View existing entries in the diary
+        view_entries(entries) # Display the entries
     else:
         print("Invalid selection, please try again.")        
 
---
+++ ProgDiary/database.py
@@ -0,0 +1,10 @@
+entries =[]
+
+def add_entry(entry_content, entry_date):
+    entries.append({"content": entry_content, "date": entry_date}) # Add entry to the list
+
+def get_entries():
+    return entries # Return the list of entries 
+    
+  
+
