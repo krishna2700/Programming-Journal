bf1a27f189b7c60637f42c08e4a255ac747aed2c
krishna2700 <krishnaruparelia0207@gmail.com>
2025-08-09 23:00:00 +0530

    connected sqlite

--- ProgDiary/database.py
+++ ProgDiary/database.py
@@ -1,4 +1,10 @@
-entries =[]
+import sqlite3
+# This module handles the database operations for the ProgDiary application
+connection = sqlite3.connect("data.db")
+
+def create_table():
+    with connection: # Create a table for diary entries if it doesn't exist
+        connection.execute("CREATE TABLE entries (content TEXT, date TEXT);") # Create a table for diary entries if it doesn't exist
 
 def add_entry(entry_content, entry_date):
     entries.append({"content": entry_content, "date": entry_date}) # Add entry to the list
